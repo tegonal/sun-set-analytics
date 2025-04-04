a66908056200dfa647c7cfb09fcc3b281695afe8
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2025-04-04 14:34:29 +0200

    access control for relationships

--- src/access/whereOwnerOrAdmin.ts
+++ src/access/whereOwnerOrAdmin.ts
@@ -1,16 +1,46 @@
+import { Access } from 'payload'
 import { ROLE_SUPER_ADMIN } from '@/utilities/constants'
-import type { PayloadRequest } from 'payload'
 
-export const isOwner = ({ req }: { req: PayloadRequest }) => {
+export const isOwner = ({ req: { user } }) => {
   return {
     owner: {
-      equals: req.user?.id,
+      equals: user?.id,
     },
   }
 }
+export const isOwnerOfReferencedInstallation: Access = ({ req }) => {
+  return {
+    'installation.owner': { equals: req.user?.id },
+  }
+}
 
-export const isAdmin = ({ req }: { req: PayloadRequest }) => {
-  if (req.user?.role === ROLE_SUPER_ADMIN) {
+export const isOwnerOfReferencedInstallationCreate: Access = async ({ req, data }) => {
+  //if (!id) {
+  //    return true
+  //}
+
+  console.debug('installation:', data)
+
+  if (!data || !data.installation) {
+    return false
+  }
+
+  // Query another Collection using the `id`
+  const result = await req.payload.find({
+    collection: 'installations',
+    limit: 0,
+    depth: 0,
+    where: {
+      id: { equals: data.installation },
+      owner: { equals: req.user?.id },
+    },
+  })
+
+  return result.totalDocs > 0
+}
+
+export const isAdmin = ({ req: { user } }) => {
+  if (user?.role === ROLE_SUPER_ADMIN) {
     return true
   }
   return false
